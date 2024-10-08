6c7bc483f77a0b305d8164b9b4565e1cc0b02de0
valex <mval.fx@gmail.com>
2024-10-08 17:45:54 +0200

    Complete 16x16 grid layout with Flexbox and add hover effect for color change on mouse enter.

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 const container = document.querySelector('.container');
 const childDiv = document.createElement('div');
-//const childDiv = document.querySelector('div');
 
 for (let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
 
         const childDiv = document.createElement('div');
         childDiv.classList.add('child-div');
-        childDiv.textContent = j;
+        //childDiv.textContent = j;
         container.appendChild(childDiv);
     }
 }
 
-// Set the hover effect when mouse enters a div
-// use event listeners
+const allChildDivs = document.querySelectorAll('.child-div');
 
-// Add event listener to each child element
-
-
-
+allChildDivs.forEach(childDiv => {
+    childDiv.addEventListener('mouseenter', () => {
+        childDiv.style.backgroundColor = "lightpink";
+    })
+})
