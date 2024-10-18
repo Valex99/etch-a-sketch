abdbd487e49bbfff97b9119de69746556bb39216
valex <mval.fx@gmail.com>
2024-10-18 23:15:49 +0200

    Improved UI design and added menu with option buttons for better user experience in Etch-a-Sketch project

--- script.js
+++ script.js
@@ -1,64 +1,83 @@
-const container = document.querySelector('.container');
-const childDiv = document.createElement('div');
-const newSquare = document.createElement('div');
+const sketchpad = document.querySelector(".sketchpad-section");
+const createGridButton = document.querySelector(".custom-grid");
+const clear = document.querySelector(".clear");
+const rainbow = document.querySelector(".rainbow");
 
-
-for (let i = 0; i < 16; i++) {
-    for(let j = 0; j < 16; j++) {
-        const childDiv = document.createElement('div');
-        childDiv.classList.add('child-div');
-        container.appendChild(childDiv);
+// Create grid 16x16 function
+function createDefaultGrid() {
+  for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+      const defaultSquare = document.createElement("div");
+      defaultSquare.classList.add("default-square");
+      sketchpad.appendChild(defaultSquare);
     }
+  }
+  // Select every element on 16x16 grid
+  const allDefaultSquares = document.querySelectorAll(".default-square");
+  allDefaultSquares.forEach((defaultSquare) => {
+    defaultSquare.addEventListener("mouseenter", () => {
+      defaultSquare.style.backgroundColor = "gray";
+    });
+  });
+}
+// Function call for default grid to be created
+createDefaultGrid();
+
+//  Get input from the user for custom grid
+createGridButton.addEventListener("click", function () {
+  let newGrid;
+  do {
+    newGrid = parseInt(prompt("Enter the number of squares per side: "));
+  } while (newGrid <= 0 || newGrid > 100);
+  deleteGrid();
+  createCustomGrid(newGrid);
+});
+
+// Clear grid button
+clear.addEventListener("click", function () {
+  deleteGrid();
+  // This will have to be modified (default grid or custom?)
+  createDefaultGrid();
+});
+
+// Function for creating new custom grid
+function createCustomGrid(newGrid) {
+  let widthSingleElement = 1296 / newGrid;
+  let heightSingleElement = 862 / newGrid;
+
+  for (let i = 0; i < newGrid; i++) {
+    for (let j = 0; j < newGrid; j++) {
+      const newSquare = document.createElement("div");
+      newSquare.classList.add("new-square");
+      newSquare.style.width = widthSingleElement + "px";
+      newSquare.style.height = heightSingleElement + "px";
+      newSquare.style.border = "1px solid black";
+      newSquare.style.backgroundColor = "lightgreen";
+      sketchpad.appendChild(newSquare);
+    }
+  }
+  stylingNewGrid();
+}
+
+function stylingNewGrid() {
+  const allNewSquares = document.querySelectorAll(".new-square");
+  allNewSquares.forEach((newSquare) => {
+    newSquare.addEventListener("mouseenter", () => {
+      // Create a variable here - user input
+      newSquare.style.backgroundColor = "orange";
+    });
+  });
 }
 
 
-const allChildDivs = document.querySelectorAll('.child-div');
+rainbow.addEventListener("click", function () {
 
 
-allChildDivs.forEach(childDiv => {
-    childDiv.addEventListener('mouseenter', () => {
-        childDiv.style.backgroundColor = "lightpink";
-    })
+
 });
-
-
-const btn = document.querySelector('.customize-button');
-
-
-btn.addEventListener('click', () => {
-    let width;
-    do {
-        width = parseInt(prompt('Enter the number of squares on each side: '));
-    }
-    while (width <= 0 || width > 100 /*|| isNaN(width)*/);
-
-    // Call function to delete entire grid
-    deleteGrid();
-    // Call function to create new grid
-    createCustomGrid(width);
-});
-
 
 function deleteGrid() {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-}
-
-
-function createCustomGrid(squaresPerSide) {
-    // Calculate border first to get the remining space (border is always 1px)
-    let remainingSpace = 960 - (squaresPerSide * 2)
-    let newWidth = remainingSpace / squaresPerSide;
-
-    for(let i = 0; i < squaresPerSide; i++) {
-        for(let j = 0; j < squaresPerSide; j++) {
-            const newSquare = document.createElement('div');
-            newSquare.classList.add('new-square');
-            newSquare.style.width = newWidth + 'px';
-            newSquare.style.height = newWidth + 'px';
-            newSquare.style.border = '1px solid black';
-            container.appendChild(newSquare);
-        }
-    }
+  while (sketchpad.firstChild) {
+    sketchpad.removeChild(sketchpad.firstChild);
+  }
 }
